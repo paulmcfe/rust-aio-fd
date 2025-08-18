84c2b4abba3f4713d476776d0937aeedc631bc30
Paul <mail@mcfedries.com>
2025-08-18 07:50:16 -0400

    Book 1, Chapter 5 examples

--- bk01ch05/temperature_converter/src/main.rs
+++ bk01ch05/temperature_converter/src/main.rs
@@ -1,30 +1,56 @@
 use std::io;
 
 fn main() {
-    show_menu();
-    let choice = get_menu_choice();
-    let temperature = get_temperature();
+    loop {
+        show_menu();
+        let choice = get_menu_choice();
+        println!();
 
-    match choice {
-        1 => println!("You chose 1!"),
-        2 => println!("You chose 2!"),
-        _ => println!("WTF is {choice}?")
+        match choice {
+            0 => {
+                println!("Thanks for using Temperature Converter!");
+                break;
+            }
+            1 => {
+                println!("Type the °F temperature you want to convert:");
+                let temperature = get_temperature();
+                if is_valid_fahrenheit(temperature) {
+                    let converted_temperature = fahrenheit_to_celsius(temperature);
+                    print_converted_temperature(temperature, "F", converted_temperature, "C");
+                } else {
+                    print_invalid_temperature(temperature, "F");
+                }
+            }
+            2 => {
+                println!("Type the °C temperature you want to convert:");
+                let temperature = get_temperature();
+                if is_valid_celsius(temperature) {
+                    let converted_temperature = celsius_to_fahrenheit(temperature);
+                    print_converted_temperature(temperature, "C", converted_temperature, "F");
+                } else {
+                    print_invalid_temperature(temperature, "C");
+                }
+            }
+            _ => println!("Invalid choice! Please try again.")
+        }
     }
 }
 
 fn show_menu() {
+    println!();
+    println!("=====================");
     println!("Temperature Converter");
     println!("=====================");
     println!("1. Convert °F to °C");
     println!("2. Convert °C to °F");
-    println!("Choose an option (1 or 2):");
+    println!("Choose an option (1 or 2; press a letter to quit):");
 }
 
 fn get_menu_choice() -> i32 {
     let mut choice = String::new();
     let _ = io::stdin().read_line(&mut choice);
-    let choice = choice.trim();
-    choice.parse().unwrap_or(0)
+    let choice = choice.trim().parse();
+    choice.unwrap_or(0)
 }
 
 fn get_temperature() -> f64 {
@@ -51,9 +77,11 @@ fn celsius_to_fahrenheit(celsius: f64) -> f64 {
 }
 
 fn print_converted_temperature(original_temp: f64, original_unit: &str, converted_temp: f64, converted_unit: &str) {
+    println!();
     println!("{:.2}°{} = {:.2}°{}", original_temp, original_unit, converted_temp, converted_unit);
 }
 
 fn print_invalid_temperature(temp: f64, unit: &str) {
+    println!();
     println!("{:.2}°{} is below absolute zero!", temp, unit);
 }
