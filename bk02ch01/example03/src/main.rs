f64da25bc1054eacf08d782673e1e898ad8b05c3
Paul <mail@mcfedries.com>
2025-08-22 08:00:07 -0400

    Book 2, Chapter 1 examples

--- bk02ch01/example03/src/main.rs
+++ bk02ch01/example03/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let s1 = String::from("Hello, world");
+    let s1 = String::from("Rumpelstiltskin");
     let s2 = s1;
     println!("The value of s1 is {s1}.");
     println!("The value of s2 is {s2}.");
--- bk02ch01/example06/src/main.rs
+++ bk02ch01/example06/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let s1 = String::from("Hello, world"); // s1 owns the String
+    let s1 = String::from("Inflatable Dartboard"); // s1 owns the String
     let s2 = &s1; // s2 borrows s1's value
     println!("The value of s1 is {s1}."); // s1 is still valid, so this works!
     println!("The value of s2 is {s2}."); // And this runs, too, no problem
