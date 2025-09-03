2c47df897e07c65a93f6cce37392942f4350f056
Paul <mail@mcfedries.com>
2025-09-03 07:53:11 -0400

    Book 2, Chapter 6 examples

--- bk02ch06/example05/src/main.rs
+++ bk02ch06/example05/src/main.rs
@@ -1,13 +1,20 @@
 fn main() {
-    // No types - let Rust figure them out
-    //let complement1 = |angle| angle + 180;
+    // Concise version - Let the compiler figure it out
+    let square = |x| x * x;
+    println!("{}", square(10)); // Output: 100
 
-    // Specify the parameter type
-    let complement2 = |angle: i32| angle + 180;
+    // Version 2: Add a type to the closure argument
+    let square2 = |x: i32| x * x;
+    println!("{}", square2(10)); // Output: 100
 
-    // Specify the parameter type and the return type
-    let complement3 = |angle: i32| -> i32 { angle + 180 };
+    // Version 3: Add types to the closure argument and result
+    let square3 = |x: i32| -> i32 { x * x };
+    println!("{}", square3(10)); // Output: 100
 
-    // Use an expression block
-    let println!("{}", complement2(30));
+    // Expression block example
+    let hypotenuse_length = |a, b| {
+        let sum_of_squares: f64 = square(a) as f64 + square(b) as f64;
+        sum_of_squares.sqrt()
+    };
+    println!("{}", hypotenuse_length(3, 4)); // Output: 5
 }
