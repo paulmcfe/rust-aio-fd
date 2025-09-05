9e519bfeb7f08b2eb6f04db030ac92b19b30a278
Paul McFedries <pmcfedries@gmail.com>
2025-09-04 21:44:26 -0400

    Book 2, Chapter 7 examples

--- bk02ch07/example04/src/user_interface/input.rs
+++ bk02ch07/example04/src/user_interface/input.rs
@@ -1,11 +1,9 @@
-pub mod input {
-    pub fn get_user_name() -> String {
-        // Actual function code goes here
-        String::from("Chase")
-    }
+pub fn get_user_name() -> String {
+    // Actual function code goes here
+    String::from("Chase")
+}
 
-    pub fn get_favorite_activity() -> String {
-        // Actual function code goes here
-        String::from("Fetch")
-    }
+pub fn get_favorite_activity() -> String {
+    // Actual function code goes here
+    String::from("Fetch")
 }
--- bk02ch07/example04/src/user_interface/output.rs
+++ bk02ch07/example04/src/user_interface/output.rs
@@ -1,9 +1,7 @@
-pub mod output {
-    pub fn display_welcome(name: &str) {
-        println!("Welcome, {name}! Are you ready to take the survey?");
-    }
+pub fn display_welcome(name: &str) {
+    println!("Welcome, {name}! Are you ready to take the survey?");
+}
 
-    pub fn display_entry(text: &str) {
-        println!("You entered {text}!");
-    }
+pub fn display_entry(text: &str) {
+    println!("You entered {text}!");
 }
